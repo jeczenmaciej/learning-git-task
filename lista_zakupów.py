c5a5df68706303ce0d3d26290324ebd6f1445150
Maciej Jęczeń <maciej.jeczen@gmail.com>
2024-04-14 17:19:22 +0200

    Init project explain what items in what shops

--- lista_zakupów.py
+++ lista_zakupów.py
@@ -2,3 +2,11 @@ lista_zakupów = {
     'piekarnia': ['chleb','pączek','bułki'],
     'warzywniak': ['marchew','seler','rukola']
 }
+
+liczba_produktów=0
+
+print ("Lista zakupów")
+for sklep,rzeczy in lista_zakupów.items():
+    liczba_produktów += len (rzeczy)
+    print (f"Idę do {sklep.capitalize()}, kupuję tu następujące rzeczy: {str([produkt.capitalize() for produkt in rzeczy])}.")
+    
