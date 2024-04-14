affd4c48e1e9f0e1aee9f2cc2d7a2ea5639c5afe
Maciej Jęczeń <maciej.jeczen@gmail.com>
2024-04-14 17:02:41 +0200

    Init project

--- lista_zakupów.py
+++ lista_zakupów.py
@@ -1,1 +1,12 @@
-Print ("Hello world!")
+lista_zakupów = {
+    'piekarnia': ['chleb','pączek','bułki'],
+    'warzywniak': ['marchew','seler','rukola']
+}
+
+liczba_produktów=0
+
+print ("Lista zakupów")
+for sklep, rzeczy in lista_zakupów.items():
+    liczba_produktów += len(rzeczy)
+    print(f"Idę do {sklep.capitalize()}, kupuję tu następujące rzeczy: {str([produkt.capitalize() for produkt in rzeczy])}.")
+print(f"W sumie kupuję {liczba_produktów} produktów.")
